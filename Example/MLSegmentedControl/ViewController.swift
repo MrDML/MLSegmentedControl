f8c5b9119ed71259fd7c07aae2423d3a0cd12927
dml <dml1630@163.com>
2018-06-15 17:55:24 +0800

    c

--- Example/MLSegmentedControl/ViewController.swift
+++ Example/MLSegmentedControl/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
+        
+        
+      
     }
 
 }
--- MLSegmentedControl/Classes/MLSegmentedControl.swift
+++ MLSegmentedControl/Classes/MLSegmentedControl.swift
@@ -9,12 +9,92 @@ import UIKit
 
 open class MLSegmentedControl: UIControl {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    
+    lazy var scrollView: UIScrollView = {
+        let scr = UIScrollView.init()
+        scr.scrollsToTop = false
+        scr.showsVerticalScrollIndicator = false
+        scr.showsHorizontalScrollIndicator = false
+        return scr
+    }()
+    
+    
+    // 条状指示器
+    lazy var selectionIndicatorStripLayer: CALayer = {
+        let stripLayer = CALayer()
+        return stripLayer
+    }()
+    
+    // 箭头指示器
+    lazy var selectionIndicatorArrowLayer: CALayer = {
+        let arrowLayer  = CALayer()
+        return arrowLayer
+    }()
+    
+    // 矩形盒指示器
+    lazy var selectionIndicatorBoxLayer: CALayer = {
+        let  boxLayer  = CALayer()
+        return boxLayer
+    }()
+    
+    
+    
+    
+    
+    // default = 0
+    var selectedSegmentIndex = 0
+    
+    
 
+    public init(titles:Array<String>,images:Array<String>) {
+        self.init()
+        initialize()
+    }
+    
+    
+    public init(titles:Array<String>) {
+        self.init()
+        initialize()
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+       super.init(coder: aDecoder)
+    }
+    
+
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        
+    }
+    
+    override open func draw(_ rect: CGRect) {
+        
+    }
+    
+    
+    func initialize(){
+      self.addSubview(scrollView)
+      self.backgroundColor = UIColor.white
+        
+        
+        
+        // 当视图的size 发生变化时 会调用 drawrect 方法
+        self.contentMode = .redraw
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
